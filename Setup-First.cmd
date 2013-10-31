6131609348171b0572973924e0a1a57e74ea7548
F.D.Castel <fdcastel@gmail.com>
2013-10-31 16:28:45 -0200

    Fix problems with ExecutionPolicy on Win 8.1
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

--- Setup-First.cmd
+++ Setup-First.cmd
@@ -1,1 +1,3 @@
-@%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -Command "& '%~dpn0.ps1' %*"
+@ECHO OFF
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -command set-executionpolicy remotesigned -scope currentuser
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -Command "& '%~dpn0.ps1' %*"
