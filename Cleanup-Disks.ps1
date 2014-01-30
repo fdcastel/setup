8d1a6da3155c8913fd5f46e9c7a00e9f2cb54ca8
F.D.Castel <fdcastel@gmail.com>
2014-01-30 04:51:33 -0200

    Fix Cleanup-Disks.ps1 / Add elevation.
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

--- Cleanup-Disks.ps1
+++ Cleanup-Disks.ps1
@@ -1,11 +1,45 @@
-# Execute all disk cleanup tasks for all disks
-#   Must run with elevated privileges
+######
+#
+# Cleanup-Disks.ps1
+#
+# fdcastel@gmail.com
+#
+###
 
-# Add StateFlags1337 for each area
-Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches" | ForEach-Object {
-    Get-ItemProperty $_.PSPath -Name StateFlags | ForEach-Object {
-        Set-ItemProperty $_.PSPath -Name StateFlags1337 -Value 2 -Type DWord -Force | Out-Null
+[CmdletBinding()]
+Param()
+
+[string[]]$innerArguments = $MyInvocation.BoundParameters.GetEnumerator() | 
+    ForEach-Object {
+        if ($_.Value -is [Switch]) {
+            "-$($_.Key)"
+        } else {
+            "-$($_.Key)", "$($_.Value)"
+        }
     }
+$innerArguments += $MyInvocation.UnboundArguments
+
+[string[]]$outerArguments = @('-NoProfile', '-File', "`"$($MyInvocation.MyCommand.Path)`"")
+$outerArguments += $innerArguments
+
+function Requires-Elevation {
+    # Restart script with administrative privileges, if needed
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+        Write-Verbose 'Running without administrative privileges. Starting new (elevated) PowerShell session...'
+        Write-Verbose "  Arguments: $($outerArguments)"
+        Start-Process -FilePath PowerShell.exe -Verb Runas -WorkingDirectory $pwd -ArgumentList $outerArguments
+        Exit
+    }
+    Write-Verbose 'Running with administrative privileges.'
+    Write-Verbose "  Arguments: $($innerArguments)"
+}
+
+# Must run with elevated privileges
+Requires-Elevation
+
+# Add StateFlags1337 for each handler (http://msdn.microsoft.com/en-us/library/bb776782(v=vs.85).aspx)
+Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches" | ForEach-Object {
+    Set-ItemProperty $_.PSPath -Name StateFlags1337 -Value 2 -Type DWord -Force | Out-Null
 }
 
 # Run Disk Cleanup
