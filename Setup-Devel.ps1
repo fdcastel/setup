917e74e0f1038428478bc6196d09477b7daf07bd
F.D.Castel <fdcastel@gmail.com>
2015-10-07 16:29:56 -0300

    Adds git credential.helper. Adds -y to chocolatey install.
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

--- Setup-Devel.ps1
+++ Setup-Devel.ps1
@@ -15,24 +15,25 @@ function Reload-Path {
 # Stop on any error
 $ErrorActionPreference = 'Stop'
 
-cinst beyondcompare 
+choco install beyondcompare -y
 
-cinst git
+choco install git -y
 Reload-Path
 git config --global user.email fdcastel@gmail.com
 git config --global user.name F.D.Castel
+git config --global credential.helper wincred
 
-cinst TortoiseGit 
+choco install tortoisegit -y
 # Configure Beyond Compare integration
 New-ItemProperty HKCU:\Software\TortoiseGit -Force -Name Diff  -PropertyType String -Value '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly' | Out-Null
 New-ItemProperty HKCU:\Software\TortoiseGit -Force -Name Merge -PropertyType String -Value '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %mine %theirs %base %merged /title1=%yname /title2=%tname /title3=%bname /title4=%mname' | Out-Null
 
-cinst svn 
+choco install svn -y
 # Set Global Ignore Pattern:
 New-Item HKCU:\Software\Tigris.org\Subversion\Config\miscellany -Force | Out-Null
 New-ItemProperty HKCU:\Software\Tigris.org\Subversion\Config\miscellany -Force -Name global-ignores -PropertyType String -Value '*.exe *.bpl *.dcp *.dcu *.~* *.map *.rsm *.log *.$$$ *.dsk *.dti *.ddp *.bdsproj.local *.bdsgroup.local *.user *.identcache __history *.pdb *.Cache *.local' | Out-Null
 
-cinst tortoisesvn 
+choco install tortoisesvn -y
 # Configure Beyond Compare integration
 New-ItemProperty HKCU:\Software\TortoiseSVN -Force -Name DiffProps -PropertyType String -Value '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly' | Out-Null
 New-ItemProperty HKCU:\Software\TortoiseSVN -Force -Name Diff      -PropertyType String -Value '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly' | Out-Null
