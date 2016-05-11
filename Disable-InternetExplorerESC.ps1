39cc2a78970955e41df15941ff6db99e2d2dcfc8
F.D.Castel <fdcastel@gmail.com>
2016-05-11 19:45:00 -0300

    Adds Disable-InternetExplorerESC.ps1.
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

---
+++ Disable-InternetExplorerESC.ps1
@@ -0,0 +1,6 @@
+# Disable Internet Explorer Enhanced Security Configuration
+
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
