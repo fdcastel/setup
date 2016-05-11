0eff37de35cafc9024949db7824e66143d37c839
F.D.Castel <fdcastel@gmail.com>
2016-05-11 19:47:06 -0300

    Updates Setup-Utils.cmd.
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

--- Setup-Utils.cmd
+++ Setup-Utils.cmd
@@ -1,7 +1,1 @@
-@ECHO OFF
-
-CALL cinst 7zip GoogleChrome TeamViewer sublimetext3 utorrent
-
-IF /I [%1] EQU [FULL] (
-	CALL cinst PdfXchangeViewer PdfCreator Skype
-)
+@choco install PdfXchangeViewer PdfCreator Skype -y
