581957aaee699db2e2ea5d88d43a12dc63481905
F.D.Castel <fdcastel@gmail.com>
2017-03-11 09:04:10 -0300

    Split Setup-Dev.ps1 in Setup-Git.ps1 and Setup-Svn.ps1.

---
+++ Setup-Git.ps1
@@ -0,0 +1,29 @@
+######
+#
+# Setup-Git.ps1
+#
+# fdcastel@gmail.com
+#
+###
+
+function Reload-Path {
+    # Updates current session PATH reading the most updated one from Registry
+    $env:Path =  (Get-ItemProperty 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment').Path + ';' + `
+                 (Get-ItemProperty 'HKCU:\Environment').Path
+}
+
+# Stop on any error
+$ErrorActionPreference = 'Stop'
+
+choco install beyondcompare -y
+
+choco install git -y
+Reload-Path
+git config --global user.email fdcastel@gmail.com
+git config --global user.name F.D.Castel
+git config --global credential.helper wincred
+
+choco install tortoisegit -y
+# Configure Beyond Compare integration
+New-ItemProperty HKCU:\Software\TortoiseGit -Force -Name Diff  -PropertyType String -Value '"C:\Program Files\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly' | Out-Null
+New-ItemProperty HKCU:\Software\TortoiseGit -Force -Name Merge -PropertyType String -Value '"C:\Program Files\Beyond Compare 4\BComp.exe" %mine %theirs %base %merged /title1=%yname /title2=%tname /title3=%bname /title4=%mname' | Out-Null
