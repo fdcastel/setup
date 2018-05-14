32c10c4313dc79b52b1314d9c6fe1e20bab4feb7
F.D.Castel <fdcastel@gmail.com>
2018-05-13 23:58:31 -0300

    Fix Disable-InternetExplorerESC.ps1.

--- Disable-InternetExplorerESC.ps1
+++ Disable-InternetExplorerESC.ps1
@@ -2,5 +2,8 @@
 
 $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
 $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+Rundll32 iesetup.dll, IEHardenLMSettings,1,True
+Rundll32 iesetup.dll, IEHardenUser,1,True
+Rundll32 iesetup.dll, IEHardenAdmin,1,True
