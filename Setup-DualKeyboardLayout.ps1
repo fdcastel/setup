1726d49a5238ca7e929ab0aee914674406590e7d
F.D.Castel <fdcastel@gmail.com>
2022-11-28 20:59:02 -0300

    Updates Setup-*KeyboardLayout.ps1. Removes Disable-InternetExplorerESC.ps1.

--- Setup-DualKeyboardLayout.ps1
+++ Setup-DualKeyboardLayout.ps1
@@ -1,33 +1,23 @@
 # Set two Keyboard Layouts: pt-BR/ABNT2, en-US/International
 
-# Determine Windows version
-$WindowsVersion = [System.Environment]::OSVersion.Version.Major * 10 + [System.Environment]::OSVersion.Version.Minor
+# Set Windows UI Language
+Set-WinUILanguageOverride 'en-US'
 
-if ($WindowsVersion -ge 62)
-{
-    # Windows 8 or higher
-    $langList = New-WinUserLanguageList pt-BR
-    $langList[0].InputMethodTips.Clear()
-    $langList[0].InputMethodTips.Add('0416:00010416')
-    $langList[0].InputMethodTips.Add('0416:00020409')
-    Set-WinUserLanguageList $langList -Force
+# Set languages and input methods 
+$LanguageList = Get-WinUserLanguageList 
+$LanguageList.Clear()
+$LanguageList.Add('pt-BR') 
+$LanguageList[0].InputMethodTips.Clear()
+$LanguageList[0].InputMethodTips.Add('0416:00010416')
+$LanguageList.Add('en-US') 
+$LanguageList[1].InputMethodTips.Clear()
+$LanguageList[1].InputMethodTips.Add('0416:00020409')
+Set-WinUserLanguageList $LanguageList -Force
 
-    # Disable hotkeys for switching input layout/language
-    $HKCUInputMethodHotKeys104 = 'HKCU:\Control Panel\Input Method\Hot Keys\00000104'
-    Remove-Item -Path $HKCUInputMethodHotKeys104 -Recurse -ErrorAction 'SilentlyContinue'
-
-    $HKCUKeyboardLayoutToggle = 'HKCU:\Keyboard Layout\Toggle\'
-    Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Language Hotkey' -Value 3
-    Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Layout Hotkey' -Value 3
-    Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Hotkey' -Value 3
-} else {
-    # Windows 7 or lower
-    $HKCUKeyboardPreload = 'HKCU:\Keyboard Layout\Preload'
-    $HKCUKeyboardSubstitutes = 'HKCU:\Keyboard Layout\Substitutes'
-    Remove-ItemProperty $HKCUKeyboardPreload -Name (Get-Item $HKCUKeyboardPreload).Property    # Remove all (dumb?) 
-    Remove-ItemProperty $HKCUKeyboardSubstitutes -Name (Get-Item $HKCUKeyboardSubstitutes).Property
-    Set-ItemProperty -path $HKCUKeyboardPreload -name '1' -value '00000416'
-    Set-ItemProperty -path $HKCUKeyboardPreload -name '2' -value '00000409'
-    Set-ItemProperty -path $HKCUKeyboardSubstitutes -name '00000409' -value '00020409'
-    Set-ItemProperty -path $HKCUKeyboardSubstitutes -name '00000416' -value '00010416'
-}
+# Disable hotkeys for switching input layout/language
+$HKCUInputMethodHotKeys104 = 'HKCU:\Control Panel\Input Method\Hot Keys\00000104'
+Remove-Item -Path $HKCUInputMethodHotKeys104 -Recurse -ErrorAction 'SilentlyContinue'
+$HKCUKeyboardLayoutToggle = 'HKCU:\Keyboard Layout\Toggle\'
+Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Language Hotkey' -Value 3
+Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Layout Hotkey' -Value 3
+Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Hotkey' -Value 3
--- Setup-SingleKeyboardLayout.ps1
+++ Setup-SingleKeyboardLayout.ps1
@@ -1,30 +1,18 @@
 # Set the only Keyboard Layout to pt-BR/ABNT2
 
-# Determine Windows version
-$WindowsVersion = [System.Environment]::OSVersion.Version.Major * 10 + [System.Environment]::OSVersion.Version.Minor
+# Set Windows UI Language
+Set-WinUILanguageOverride 'en-US'
 
-if ($WindowsVersion -ge 62)
-{
-    # Windows 8 or higher
-    $langList = New-WinUserLanguageList pt-BR
-    $langList[0].InputMethodTips.Clear()
-    $langList[0].InputMethodTips.Add('0416:00010416')
-    Set-WinUserLanguageList $langList -Force
+# Set language and input methods 
+$langList = New-WinUserLanguageList pt-BR
+$langList[0].InputMethodTips.Clear()
+$langList[0].InputMethodTips.Add('0416:00010416')
+Set-WinUserLanguageList $langList -Force
 
-    # Disable hotkeys for switching input layout/language
-    $HKCUInputMethodHotKeys104 = 'HKCU:\Control Panel\Input Method\Hot Keys\00000104'
-    Remove-Item -Path $HKCUInputMethodHotKeys104 -Recurse -ErrorAction 'SilentlyContinue'
-
-    $HKCUKeyboardLayoutToggle = 'HKCU:\Keyboard Layout\Toggle\'
-    Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Language Hotkey' -Value 3
-    Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Layout Hotkey' -Value 3
-    Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Hotkey' -Value 3
-} else {
-    # Windows 7 or lower
-    $HKCUKeyboardPreload = 'HKCU:\Keyboard Layout\Preload'
-    $HKCUKeyboardSubstitutes = 'HKCU:\Keyboard Layout\Substitutes'
-    Remove-ItemProperty $HKCUKeyboardPreload -Name (Get-Item $HKCUKeyboardPreload).Property    # Remove all (dumb?) 
-    Remove-ItemProperty $HKCUKeyboardSubstitutes -Name (Get-Item $HKCUKeyboardSubstitutes).Property
-    Set-ItemProperty -path $HKCUKeyboardPreload -name '1' -value '00000416'
-    Set-ItemProperty -path $HKCUKeyboardSubstitutes -name '00000416' -value '00010416'
-}
+# Disable hotkeys for switching input layout/language
+$HKCUInputMethodHotKeys104 = 'HKCU:\Control Panel\Input Method\Hot Keys\00000104'
+Remove-Item -Path $HKCUInputMethodHotKeys104 -Recurse -ErrorAction 'SilentlyContinue'
+$HKCUKeyboardLayoutToggle = 'HKCU:\Keyboard Layout\Toggle\'
+Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Language Hotkey' -Value 3
+Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Layout Hotkey' -Value 3
+Set-ItemProperty -Path $HKCUKeyboardLayoutToggle -Name 'Hotkey' -Value 3
