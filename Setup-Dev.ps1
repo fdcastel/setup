f385dab2ab716faaf46286e560d48563884689ad
F.D.Castel <fdcastel@gmail.com>
2023-05-04 17:41:25 -0300

    Adds Visual Studio Code.

--- Setup-Dev.ps1
+++ Setup-Dev.ps1
@@ -1,6 +1,6 @@
 ######
 #
-# Setup-Git.ps1
+# Setup-Dev.ps1
 #
 # fdcastel@gmail.com
 #
@@ -40,3 +40,6 @@ choco install tortoisegit -y
 # Configure TortoiseGit / Beyond Compare integration
 New-ItemProperty HKCU:\Software\TortoiseGit -Force -Name Diff  -PropertyType String -Value '"C:\Program Files\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly' | Out-Null
 New-ItemProperty HKCU:\Software\TortoiseGit -Force -Name Merge -PropertyType String -Value '"C:\Program Files\Beyond Compare 4\BComp.exe" %mine %theirs %base %merged /title1=%yname /title2=%tname /title3=%bname /title4=%mname' | Out-Null
+
+# Install Visual Studio Code
+choco install vscode -y
