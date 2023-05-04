8222b4ddbe0e3e2189ab18ca678d836133030cf7
F.D.Castel <fdcastel@gmail.com>
2023-05-04 17:41:49 -0300

    Adds Setup-Python.ps1.

---
+++ Setup-Python.ps1
@@ -0,0 +1,61 @@
+######
+#
+# Setup-Python.ps1
+#
+# fdcastel@gmail.com
+#
+###
+
+function Reload-Path {
+    # Updates current session PATH reading the most updated one from Registry
+    $env:Path =  (Get-ItemProperty 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment').Path + ';' + `
+                 (Get-ItemProperty 'HKCU:\Environment').Path
+}
+
+# Source: https://stackoverflow.com/a/34844707
+function Add-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session'
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
+        if ($persistedPaths -notcontains $Path) {
+            $persistedPaths = $persistedPaths + $Path | where { $_ }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split ';'
+    if ($envPaths -notcontains $Path) {
+        $envPaths = $envPaths + $Path | where { $_ }
+        $env:Path = $envPaths -join ';'
+    }
+}
+
+# Stop on any error
+$ErrorActionPreference = 'Stop'
+
+# Install Python 
+choco install python -y
+Reload-Path
+
+# Update Pip
+python.exe -m pip install --upgrade pip
+
+# Install Poetry (unofficial, disables SSL certificate validation)
+(Invoke-WebRequest -Uri https://raw.githubusercontent.com/fdcastel/install-poetry/main/install-poetry.py -UseBasicParsing).Content | python.exe -
+
+Add-EnvPath -Path "$env:APPDATA\Python\Scripts" -Container User
+Reload-Path
+poetry config virtualenvs.in-project true
